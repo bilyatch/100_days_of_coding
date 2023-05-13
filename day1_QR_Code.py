8c44cbaf60b09672ebb59009aad1be87b5518116
bilyatch <104844487+bilyatch@users.noreply.github.com>
2023-05-13 08:34:04 +0545

    Update day1_QR_Code.py

--- day1_QR_Code.py
+++ day1_QR_Code.py
@@ -1,8 +1,8 @@
 import qrcode
 from PIL import Image
 qr=qrcode.QRCode(version=1,error_correction= qrcode.constants.ERROR_CORRECT_H, box_size=10, border= 4)
-qr.add_data("https://www.instagram.com/yogisha._/")
+qr.add_data("https://www.linkedin.com/in/samir-shrestha-95403b240")
 qr.make(fit=True)
 img=qr.make_image(fill_color="red",back_color="black")
-img.save("baby_insta.png")
+img.save("Do_visit.png")
 
